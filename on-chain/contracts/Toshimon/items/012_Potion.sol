0b33b0ce067a21e281925a5fd543e604b7bc79f5
Willem Olding <willemolding@gmail.com>
2022-07-05 14:03:18 +1000

    fix option heal value

--- on-chain/contracts/Toshimon/items/012_Potion.sol
+++ on-chain/contracts/Toshimon/items/012_Potion.sol
@@ -9,11 +9,13 @@ import { ToshimonState as TM } from '../ToshimonState.sol';
 import { IItem } from '../interfaces/IItem.sol';
 
 contract Potion is IItem {
+	uint8 constant heals = 25;
+
 	function applyItem(TM.GameState memory state, uint8 mover, uint8 monster, bytes32 randomSeed) external override pure returns (TM.GameState memory) {
-		// add 30 HP to the receiver up to their max HP
+		// add 25 HP to the receiver up to their max HP
 		uint8 currentHp = state.players[mover].monsters[monster].stats.hp;
 		uint8 maxHp = state.players[mover].monsters[monster].baseStats.hp;
-		state.players[mover].monsters[monster].stats.hp = uint8(Math.min(uint256(currentHp + 30), uint256(maxHp)));
+		state.players[mover].monsters[monster].stats.hp = uint8(Math.min(uint256(currentHp + heals), uint256(maxHp)));
 		return state;
 	}
 }
